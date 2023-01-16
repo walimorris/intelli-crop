a29767cf6472cd5e8b573e1362da5f82fa4a7a38
Wali Morris <walimmorris@gmail.com>
2023-01-15 16:12:36 -0800

    refactor InvokeIntelliCropWorkFlow.java and DetectBoundingBoxImageMetaData.java

--- src/main/java/com/morris/intellicrop/events/DetectBoundingBoxImageMetaData.java
+++ src/main/java/com/morris/intellicrop/events/DetectBoundingBoxImageMetaData.java
@@ -5,44 +5,101 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
-import com.amazonaws.services.rekognition.model.*;
+import com.amazonaws.services.rekognition.model.BoundingBox;
+import com.amazonaws.services.rekognition.model.ComparedFace;
+import com.amazonaws.services.rekognition.model.RecognizeCelebritiesResult;
+import com.amazonaws.services.rekognition.model.RecognizeCelebritiesRequest;
+import com.amazonaws.services.rekognition.model.S3Object;
+import com.amazonaws.services.rekognition.model.Image;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class DetectBoundingBoxImageMetaData {
+    public static final String BUCKET = "bucket";
+    public static final String KEY = "key";
+    public static final String TOP = "top";
+    public static final String LEFT = "left";
+    public static final String HEIGHT = "height";
+    public static final String WIDTH = "width";
 
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("Detect Image Meta Data Function in StateMachine has been triggered");
+        logger.log("Detect Image Meta Data Function in Intelli-Crop StateMachine has been triggered");
         Regions region = Regions.fromName(System.getenv("REGION"));
 
+        BoundingBox boundingBox = null;
         if (input != null) {
-            String bucket = (String) input.get("bucket");
-            String key = (String) input.get("key");
+            String bucket = (String) input.get(BUCKET);
+            String key = (String) input.get(KEY);
 
             AmazonRekognition amazonRekognitionClient = AmazonRekognitionClientBuilder
                     .standard()
                     .withRegion(region)
                     .build();
 
-            RecognizeCelebritiesRequest request = new RecognizeCelebritiesRequest()
-                    .withImage(new Image().withS3Object(new S3Object().withName(key).withBucket(bucket)));
-
-            RecognizeCelebritiesResult result = amazonRekognitionClient.recognizeCelebrities(request) ;
-            ComparedFace face = result.getUnrecognizedFaces().get(0);
-            BoundingBox boundingBox = face.getBoundingBox();
-
-            Map<String, String> output = new HashMap<>();
-            if (boundingBox != null) {
-                output.put("top", String.valueOf(boundingBox.getTop()));
-                output.put("height", String.valueOf(boundingBox.getHeight()));
-                output.put("left", String.valueOf(boundingBox.getLeft()));
-                output.put("width", String.valueOf(boundingBox.getWidth()));
-            }
-            return output;
-
+            boundingBox = analyzeImageForBoundingBox(amazonRekognitionClient, bucket, key, logger);
         }
+        return buildStepFunctionOutput(boundingBox, logger);
+    }
+
+    /**
+     * Utilizes the Amazon Rekognition {@link AmazonRekognition} machine learning algorithm to analyze
+     * the given image and produces a bounding box of the face in the image.
+     *
+     * @param rekognitionClient {@link AmazonRekognition}
+     * @param bucket S3 input bucket
+     * @param key S3 image key
+     * @param logger {@link LambdaLogger}
+     *
+     * @return {@link BoundingBox}
+     */
+    private BoundingBox analyzeImageForBoundingBox(AmazonRekognition rekognitionClient, String bucket, String key,
+                                                   LambdaLogger logger) {
+
+        RecognizeCelebritiesRequest request = new RecognizeCelebritiesRequest()
+                .withImage(new Image().withS3Object(new S3Object()
+                        .withName(key)
+                        .withBucket(bucket)));
+
+        RecognizeCelebritiesResult result = rekognitionClient.recognizeCelebrities(request);
+
+        ComparedFace face = null;
+
+        // check to ensure uploaded image is not a celebrity
+        if (!result.getUnrecognizedFaces().isEmpty()) {
+            face = result.getUnrecognizedFaces().get(0);
+
+        // check if given image is a celebrity
+        } else if (!result.getCelebrityFaces().isEmpty()) {
+            face = result.getCelebrityFaces().get(0).getFace();
+        } else {
+            logger.log("Rekognition can not provide a bounding box attributes from uploaded image");
+            return null;
+        }
+        return face.getBoundingBox();
+    }
+
+    /**
+     * Builds Intelli-Crop Step Function output for the DetectBoundingBox portion of the intelli-crop
+     * statemachine workflow or gives a clear message if bounding-box does not exist.
+     *
+     * @param boundingBox {@link BoundingBox}
+     * @param logger {@link LambdaLogger}
+     *
+     * @return {@link Map} of bounding-box properties
+     */
+    private Map<String, String> buildStepFunctionOutput(BoundingBox boundingBox, LambdaLogger logger) {
+        Map<String, String> output = new HashMap<>();
+        if (boundingBox != null) {
+            output.put(TOP, String.valueOf(boundingBox.getTop()));
+            output.put(HEIGHT, String.valueOf(boundingBox.getHeight()));
+            output.put(LEFT, String.valueOf(boundingBox.getLeft()));
+            output.put(WIDTH, String.valueOf(boundingBox.getWidth()));
+
+            return output;
+        }
+        logger.log("Empty bounding-box results for uploaded image in intelli-crop workflow");
         return null;
     }
 }
--- src/main/java/com/morris/intellicrop/events/InvokeIntelliCropWorkFlow.java
+++ src/main/java/com/morris/intellicrop/events/InvokeIntelliCropWorkFlow.java
@@ -8,12 +8,19 @@ import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
 import com.amazonaws.services.stepfunctions.AWSStepFunctionsClient;
 import com.amazonaws.services.stepfunctions.AWSStepFunctionsClientBuilder;
-import com.amazonaws.services.stepfunctions.model.*;
-import org.json.JSONObject;
+import com.amazonaws.services.stepfunctions.model.ListStateMachinesRequest;
+import com.amazonaws.services.stepfunctions.model.ListStateMachinesResult;
+import com.amazonaws.services.stepfunctions.model.StateMachineListItem;
+import com.amazonaws.services.stepfunctions.model.StartExecutionRequest;
+import com.amazonaws.services.stepfunctions.model.StartExecutionResult;
 
+import org.json.JSONObject;
 import java.util.List;
 
 public class InvokeIntelliCropWorkFlow {
+    public static final String BUCKET = "bucket";
+    public static final String KEY = "key";
+    public static final String STATE_MACHINE_ARN = "stateMachineArn";
 
     public String handleRequest(S3Event event, Context context) {
         LambdaLogger logger = context.getLogger();
@@ -27,42 +34,68 @@ public class InvokeIntelliCropWorkFlow {
                 .withRegion(region);
 
         AWSStepFunctionsClient stepFunctionsClient = (AWSStepFunctionsClient) stepFunctionsClientBuilder.build();
+        String intelliCropStateMachineArn = getIntelliCropStateMachineArn(stepFunctionsClient, logger);
 
-        // List State Machines and collect intelli crop State Machine
-        String intelliCropStateMachineArn = null;
+        if (intelliCropStateMachineArn != null && bucket != null && key != null) {
+            startIntelliCropWorkflow(stepFunctionsClient, intelliCropStateMachineArn, bucket, key, logger, context);
+        } else {
+            logger.log("Check that this function consumes the correct workflow input for a successful process.");
+        }
+
+        stepFunctionsClient.shutdown();
+        return "success";
+    }
+
+    /**
+     * Uses the {@link AWSStepFunctionsClient} to query AWS Step Functions Service for the intelli-crop
+     * statemachine ARN, given the statemachine name.
+     *
+     * @param stepFunctionsClient {@link AWSStepFunctionsClient}
+     * @param logger {@link LambdaLogger}
+     *
+     * @return {@link String} intelli-crop statemachine ARN
+     */
+    private String getIntelliCropStateMachineArn(AWSStepFunctionsClient stepFunctionsClient, LambdaLogger logger) {
         try {
-
             ListStateMachinesResult listStateMachinesResult = stepFunctionsClient.listStateMachines(new ListStateMachinesRequest());
             List<StateMachineListItem> stateMachines = listStateMachinesResult.getStateMachines();
             if (!stateMachines.isEmpty()) {
                 for (StateMachineListItem statemachine : stateMachines) {
                     if (statemachine.getName().equals(System.getenv("STATE_MACHINE"))) {
-                        intelliCropStateMachineArn = statemachine.getStateMachineArn();
-                        logger.log("StateMachine: " + statemachine.getName());
-                        logger.log("StateMachineArn: " + statemachine.getStateMachineArn());
+                        return statemachine.getStateMachineArn();
                     }
                 }
             }
         } catch (AmazonServiceException e) {
-            logger.log("Error processing intelli-crop statemachine step functions request");
+            logger.log("Error processing intelli-crop statemachine step functions request: " + e.getErrorMessage());
         }
+        return null;
+    }
 
-        // invoke intelli crop statement step functions workflow and pass bucket and key to first
-        // lambda function in the workflow
-        if (intelliCropStateMachineArn != null && bucket != null && key != null) {
-            JSONObject stepFunctionsInput = new JSONObject();
-            stepFunctionsInput.put("bucket", bucket);
-            stepFunctionsInput.put("key", key);
-            stepFunctionsInput.put("stateMachineArn", intelliCropStateMachineArn);
+    /**
+     * Initiates the intelli-crop Step Function Workflow given the input from a user uploading an image
+     * to the intelli-crop image upload bucket.
+     *
+     * @param stepFunctionsClient {@link AWSStepFunctionsClient}
+     * @param intelliCropStateMachineArn intelli-crop statemachine arn
+     * @param bucket intelli-crop input bucket
+     * @param key uploaded S3 image object key
+     * @param logger {@link LambdaLogger}
+     * @param context {@link Context}
+     */
+    private void startIntelliCropWorkflow(AWSStepFunctionsClient stepFunctionsClient, String intelliCropStateMachineArn, String bucket, String key,
+                                          LambdaLogger logger, Context context) {
 
-            StartExecutionRequest startStepFunctionsExecutionRequest = new StartExecutionRequest()
-                    .withStateMachineArn(intelliCropStateMachineArn)
-                    .withInput(stepFunctionsInput.toString());
+        JSONObject stepFunctionsInput = new JSONObject();
+        stepFunctionsInput.put(BUCKET, bucket);
+        stepFunctionsInput.put(KEY, key);
+        stepFunctionsInput.put(STATE_MACHINE_ARN, intelliCropStateMachineArn);
 
-            StartExecutionResult startStepFunctionsExecutionResult = stepFunctionsClient.startExecution(startStepFunctionsExecutionRequest);
-            logger.log("Function [" + context.getFunctionName() + "], Result: " + startStepFunctionsExecutionResult.toString());
-        }
+        StartExecutionRequest startStepFunctionsExecutionRequest = new StartExecutionRequest()
+                .withStateMachineArn(intelliCropStateMachineArn)
+                .withInput(stepFunctionsInput.toString());
 
-        return "success";
+        StartExecutionResult startStepFunctionsExecutionResult = stepFunctionsClient.startExecution(startStepFunctionsExecutionRequest);
+        logger.log("Function [" + context.getFunctionName() + "], Result: " + startStepFunctionsExecutionResult.toString());
     }
 }
